b22548bbe1b2c7f174705db8c112f2b5b7944a92
Jatin JD <51241812+JatinJD14296@users.noreply.github.com>
2024-04-23 16:20:21 +1000

    UI Done

---
+++ src/components/Elements/ClientSlider.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Slider from "react-slick";
+import styled from "styled-components";
+// Assets
+
+
+export default function ClientSlider() {
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
